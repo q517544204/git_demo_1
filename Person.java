d6c680c90050e62122651244032b5452dd463e8c
Feige Sun <517544204@qq.com>
2019-12-26 21:07:40 +0800

    add Person.java

---
+++ Person.java
@@ -0,0 +1,16 @@
+//coding=utf8
+/*************************************************************************
+ @Author: Sun Jianhui
+ @Created Time : Thu Dec 26 21:00:42 2019
+
+ @File Name: Person.java
+ @Description:
+
+ ************************************************************************/
+public class Person{
+
+	int id;
+	String name;
+	String job;
+	double salary;
+}	
